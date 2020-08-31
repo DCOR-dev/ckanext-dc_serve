0e6237c3c43bd8873b603891a86694d2989c09cd
Paul Müller <dev@craban.de>
2020-08-31 15:34:38 +0200

    initial working state for CKAN 2.9

---
+++ ckanext/dc_serve/plugin.py
@@ -0,0 +1,46 @@
+import ckan.plugins as p
+import ckan.plugins.toolkit as toolkit
+
+from dcor_shared import get_dataset_path
+from flask import Blueprint
+
+from .jobs import generate_condensed_dataset_job
+from .route_funcs import dccondense
+from .serve import dcserv
+
+
+class DCServePlugin(p.SingletonPlugin):
+    p.implements(p.IBlueprint)
+    p.implements(p.IResourceController, inherit=True)
+    p.implements(p.IActions, inherit=True)
+
+    # IBlueprint
+    def get_blueprint(self):
+        u'''Return a Flask Blueprint object to be registered by the app.'''
+
+        # Create Blueprint for plugin
+        blueprint = Blueprint(self.name, self.__module__)
+
+        # Add plugin url rules to Blueprint object
+        rules = [
+            ('/dataset/<uuid:id>/resource/<uuid:resource_id>/condensed.rtdc',
+             'dccondense',
+             dccondense),
+        ]
+        for rule in rules:
+            blueprint.add_url_rule(*rule)
+        return blueprint
+
+    # IResourceController
+    def after_create(self, context, resource):
+        """Generate preview image and html file"""
+        path = get_dataset_path(context, resource)
+        toolkit.enqueue_job(generate_condensed_dataset_job,
+                            [path, resource],
+                            title="Create condensed dataset",
+                            rq_kwargs={"timeout": 3600})
+
+    # IActions
+    def get_actions(self):
+        # Registers the custom API method defined above
+        return {'dcserv': dcserv}
---
+++ ckanext/dc_serve/tests/test_plugin.py
@@ -0,0 +1,6 @@
+"""Tests for plugin.py."""
+import ckanext.dc_serve.plugin as plugin
+
+
+def test_plugin():
+    pass
