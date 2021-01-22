3baabde61c76276a9952a7e5f021acb15a096dd1
Paul Müller <dev@craban.de>
2021-01-22 15:56:14 +0100

    ci: gh actions and setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from os.path import dirname, realpath, exists
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from setuptools import setup, find_packages
 from codecs import open  # To use a consistent encoding
 import sys
 
@@ -10,7 +10,7 @@ name = 'ckanext-dc_serve'
 year = "2020"
 
 sys.path.insert(0, realpath(dirname(__file__))+"/" + "/".join(name.split("-")))
-from _version import version
+from _version import version  # noqa: E402
 
 
 setup(
@@ -27,10 +27,9 @@ setup(
     package_dir={name: name},
     namespace_packages=['ckanext'],
     install_requires=[
-        # CKAN extensions should not list dependencies here, but in a separate
-        # ``requirements.txt`` file.
-        #
-        # http://docs.ckan.org/en/latest/extensions/best-practices.html#add-third-party-libraries-to-requirements-txt
+        # the "ckan" dependency is implied
+        "dclab>=0.27.11",
+        "dcor_shared>=0.2.0",
     ],
     include_package_data=True,
     # To provide executable scripts, use entry points in preference to the
