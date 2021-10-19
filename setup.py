626c173ad5b5dc98d92161da37065a7a3647ad8a
Paul Müller <dev@craban.de>
2021-10-19 11:19:50 +0200

    setup: bump dclab from 0.35.6 to 0.36.1 (improved writing speed)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     namespace_packages=['ckanext'],
     install_requires=[
         # the "ckan" dependency is implied
-        "dclab>=0.35.6",
+        "dclab>=0.36.1",
         "dcor_shared>=0.2.0",
     ],
     include_package_data=True,
