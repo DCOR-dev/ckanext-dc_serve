277ba7320edf8fa4b70748f2cbe1616a169b5b1f
Paul Müller <dev@craban.de>
2025-06-30 21:02:14 +0200

    tests: wrong feature name

--- ckanext/dc_serve/tests/test_jobs.py
+++ ckanext/dc_serve/tests/test_jobs.py
@@ -221,4 +221,4 @@ def test_upload_condensed_dataset_to_s3_job_and_verify_intra_dataset_basin(
         assert "userdef3" in ds.features
         assert "userdef3" in ds.features_basin
         assert "userdef3" not in ds.features_innate
-        assert np.all(ds["userdef1"] == np.arange(2, 10))
+        assert np.all(ds["userdef3"] == np.arange(2, 10))
