5f2bdcb478459359bff0a655a13228c2941b6f86
Paul Müller <dev@craban.de>
2025-07-09 10:36:22 +0200

    tests: correct basin mapping strings

--- ckanext/dc_serve/tests/test_jobs.py
+++ ckanext/dc_serve/tests/test_jobs.py
@@ -553,7 +553,7 @@ def test_upload_condensed_dataset_to_s3_job_and_verify_intra_dataset_basin_chn(
         print("BASINS2", basins)
         for bn_dict in basins:
             if bn_dict["name"].count("DCOR intra-dataset for anakin.rtdc"):
-                assert bn_dict["mapping"] == "same"
+                assert bn_dict["mapping"] == "basinmap0"
                 assert "userdef3" in bn_dict["features"]
                 break
         else:
@@ -573,7 +573,7 @@ def test_upload_condensed_dataset_to_s3_job_and_verify_intra_dataset_basin_chn(
         print("BASINS3", basins)
         for bn_dict in basins:
             if bn_dict["name"].count("DCOR intra-dataset for luke.rtdc"):
-                assert bn_dict["mapping"] == "basinmap0"
+                assert bn_dict["mapping"] == "basinmap1"
                 assert "userdef3" in bn_dict["features"]
                 break
         else:
